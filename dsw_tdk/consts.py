0e66dc6f8c98e6be16da1132c99e8013d709ddd9
Marek Suchánek <marek.suchanek@gmail.com>
2022-06-01 08:57:05 +0200

    Release 3.12.0

--- dsw_tdk/consts.py
+++ dsw_tdk/consts.py
@@ -2,8 +2,8 @@ import pathspec  # type: ignore
 import re
 
 APP = 'dsw-tdk'
-VERSION = '3.11.0'
-METAMODEL_VERSION = 9
+VERSION = '3.12.0'
+METAMODEL_VERSION = 10
 
 REGEX_SEMVER = re.compile(r'^[0-9]+\.[0-9]+\.[0-9]+$')
 REGEX_ORGANIZATION_ID = re.compile(r'^(?![.])(?!.*[.]$)[a-zA-Z0-9.]+$')
